a94bb3eb4d05f7b2fc7670981fcf6eadb290475d
liskarmw <948378780@qq.com>
2022-06-29 15:45:53 +0800

    Site updated: 2022-06-29 15:45:53

--- js/utils.js
+++ js/utils.js
@@ -1,203 +1,66 @@
-/* global Fluid, CONFIG */
+Star.utils = Star.$u = {
+  // Wrap images with fancybox support.
+  wrapImageWithFancyBox: function () {
+    $('.post img')
+      .not(':hidden')
+      .each(function () {
+        var $img = $(this)
+        var imgTitle = $img.attr('title') || $img.attr('alt')
+        var $imgWrap = $img.parent('a')
+        var imgSource = ['data-src', 'data-original', 'src']
+        var imgSrc = ''
 
-window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
-
-Fluid.utils = {
-
-  listenScroll: function(callback) {
-    var dbc = new Debouncer(callback);
-    window.addEventListener('scroll', dbc, false);
-    dbc.handleEvent();
-    return dbc;
-  },
-
-  unlistenScroll: function(callback) {
-    window.removeEventListener('scroll', callback);
-  },
-
-  scrollToElement: function(target, offset) {
-    var of = jQuery(target).offset();
-    if (of) {
-      jQuery('html,body').animate({
-        scrollTop: of.top + (offset || 0),
-        easing   : 'swing'
-      });
-    }
-  },
-
-  elementVisible: function(element, offsetFactor) {
-    offsetFactor = (offsetFactor && offsetFactor >= 0) ? offsetFactor : 0;
-    var rect = element.getBoundingClientRect();
-    var height = window.innerHeight || document.documentElement.clientHeight;
-    var top = rect.top;
-    return (top >= 0 && top <= height * (offsetFactor + 1))
-      || (top <= 0 && top >= -(height * offsetFactor) - rect.height);
-  },
-
-  waitElementVisible: function(selectorOrElement, callback, offsetFactor) {
-    var runningOnBrowser = typeof window !== 'undefined';
-    var isBot = (runningOnBrowser && !('onscroll' in window)) ||
-      (typeof navigator !== 'undefined' && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent));
-    if (!runningOnBrowser || isBot) {
-      return;
-    }
-
-    offsetFactor = (offsetFactor && offsetFactor >= 0) ? offsetFactor : 0;
-
-    function waitInViewport(element) {
-      if (Fluid.utils.elementVisible(element, offsetFactor)) {
-        callback();
-        return;
-      }
-      if ('IntersectionObserver' in window) {
-        var io = new IntersectionObserver(function(entries, ob) {
-          if (entries[0].isIntersecting) {
-            callback();
-            ob.disconnect();
+        if (!$imgWrap[0]) {
+          for (var i = 0; i < imgSource.length; i++) {
+            if ($img.attr(imgSource[i])) {
+              imgSrc = $img.attr(imgSource[i])
+              break
+            }
           }
-        }, {
-          threshold : [0],
-          rootMargin: (window.innerHeight || document.documentElement.clientHeight) * offsetFactor + 'px'
-        });
-        io.observe(element);
-      } else {
-        var wrapper = Fluid.utils.listenScroll(function() {
-          if (Fluid.utils.elementVisible(element, offsetFactor)) {
-            Fluid.utils.unlistenScroll(wrapper);
-            callback();
-          }
-        });
-      }
-    }
-
-    if (typeof selectorOrElement === 'string') {
-      this.waitElementLoaded(selectorOrElement, function(element) {
-        waitInViewport(element);
-      });
-    } else {
-      waitInViewport(selectorOrElement);
-    }
-  },
-
-  waitElementLoaded: function(selector, callback) {
-    var runningOnBrowser = typeof window !== 'undefined';
-    var isBot = (runningOnBrowser && !('onscroll' in window)) ||
-      (typeof navigator !== 'undefined' && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent));
-    if (!runningOnBrowser || isBot) {
-      return;
-    }
-
-    if ('MutationObserver' in window) {
-      var mo = new MutationObserver(function(records, ob) {
-        var ele = document.querySelector(selector);
-        if (ele) {
-          callback(ele);
-          ob.disconnect();
-        }
-      });
-      mo.observe(document, { childList: true, subtree: true });
-    } else {
-      document.addEventListener('DOMContentLoaded', function() {
-        var waitLoop = function() {
-          var ele = document.querySelector(selector);
-          if (ele) {
-            callback(ele);
+          $imgWrap = $img
+            .wrap(`<a class="fancybox" href="${imgSrc}"></a>`)
+            .parent('a')
+          if ($img.is('.gallery img')) {
+            $imgWrap.attr('data-fancybox', 'gallery')
           } else {
-            setTimeout(waitLoop, 100);
+            $imgWrap.attr('data-fancybox', 'default')
           }
-        };
-        waitLoop();
-      });
-    }
+        }
+        if (imgTitle) {
+          $imgWrap.attr('title', imgTitle).attr('data-caption', imgTitle)
+        }
+      })
+
+    $().fancybox({
+      selector: '[data-fancybox]',
+      loop: true,
+      transitionEffect: 'slide',
+      hash: false,
+      buttons: [
+        'share',
+        'slideShow',
+        'fullScreen',
+        'download',
+        'thumbs',
+        'close'
+      ]
+    })
   },
 
-  createScript: function(url, onload) {
-    var s = document.createElement('script');
-    s.setAttribute('src', url);
-    s.setAttribute('type', 'text/javascript');
-    s.setAttribute('charset', 'UTF-8');
-    s.async = false;
-    if (typeof onload === 'function') {
-      if (window.attachEvent) {
-        s.onreadystatechange = function() {
-          var e = s.readyState;
-          if (e === 'loaded' || e === 'complete') {
-            s.onreadystatechange = null;
-            onload();
-          }
-        };
-      } else {
-        s.onload = onload;
-      }
-    }
-    var e = document.getElementsByTagName('script')[0]
-    || document.getElementsByTagName('head')[0]
-    || document.head || document.documentElement;
-    e.parentNode.insertBefore(s, e);
-  },
-
-  createCssLink: function(url) {
-    var l = document.createElement('link');
-    l.setAttribute('rel', 'stylesheet');
-    l.setAttribute('type', 'text/css');
-    l.setAttribute('href', url);
-    var e = document.getElementsByTagName('link')[0]
-    || document.getElementsByTagName('head')[0]
-    || document.head || document.documentElement;
-    e.parentNode.insertBefore(l, e);
-  },
-
-  loadComments: function(selectors, loadFunc) {
-    var ele = document.querySelector('#comments[lazyload]');
-    if (ele) {
-      var callback = function() {
-        loadFunc();
-        ele.removeAttribute('lazyload');
-      };
-      Fluid.utils.waitElementVisible(selectors, callback, CONFIG.lazyload.offset_factor);
-    } else {
-      loadFunc();
-    }
+  // Init tocbot.
+  initTocbot: function () {
+    tocbot.init({
+      // Where to render the table of contents.
+      tocSelector: '.js-toc',
+      // Where to grab the headings to build the table of contents.
+      contentSelector: '.post-content',
+      // Which headings to grab inside of the contentSelector element.
+      headingSelector: 'h1, h2, h3, h4',
+      // For headings inside relative or absolute positioned containers within content.
+      hasInnerContainers: false,
+      // orderedList can be set to false to generate unordered lists (ul)
+      // instead of ordered lists (ol)
+      orderedList: false,
+    })
   }
-
-};
-
-/**
- * Handles debouncing of events via requestAnimationFrame
- * @see http://www.html5rocks.com/en/tutorials/speed/animations/
- * @param {Function} callback The callback to handle whichever event
- */
-function Debouncer(callback) {
-  this.callback = callback;
-  this.ticking = false;
 }
-Debouncer.prototype = {
-  constructor: Debouncer,
-
-  /**
-   * dispatches the event to the supplied callback
-   * @private
-   */
-  update: function() {
-    this.callback && this.callback();
-    this.ticking = false;
-  },
-
-  /**
-   * ensures events don't get stacked
-   * @private
-   */
-  requestTick: function() {
-    if (!this.ticking) {
-      requestAnimationFrame(this.rafCallback || (this.rafCallback = this.update.bind(this)));
-      this.ticking = true;
-    }
-  },
-
-  /**
-   * Attach this as the event listeners
-   */
-  handleEvent: function() {
-    this.requestTick();
-  }
-};
